dc1e6886e61455c59d0b92bd6ed13733c5e8cd4d
cocq cedric <cedric-developpeur-web@laposte.net>
2024-08-06 11:41:25 +0200

    1er bug corriger redirection des liens de navigation + installation de react-router-dom pour la création du routeur

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 /* istanbul ignore file */
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.scss";
 import Page from "./pages/Home";
 import { DataProvider } from "./contexts/DataContext";
@@ -6,7 +8,11 @@ import { DataProvider } from "./contexts/DataContext";
 function App() {
   return (
     <DataProvider>
-      <Page />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Page />} />
+        </Routes>
+      </Router>
     </DataProvider>
   );
 }
